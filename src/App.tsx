358d17076dd8f7e7b634ecbdb9c2e224ffab2f46
PeDro0210 <pedroruben0210@gmail.com>
2025-01-25 23:03:51 -0600

    cute message added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import AdPart from "./components/main_components/ad_part/ad_part"
 import UpperWindow from "./components/main_components/upper_window/upper_window"
 import WindowGrid from "./components/main_components/window_grid/window_grid"
 import "./App.css"
+import { useEffect } from "react"
 
 //TODO: comment this one day
 function App() {
-
+  useEffect(() =>
+    console.log("Amor, no tienes ni idea de cuanto te amo, estos meses han sido unicos y queria expresarte mi Amor con algo para sacar mi creatividad adelante y darte la razon en que si soy alguien asi, Te amo mi amor eres unica hermosa y perfecta, ahorita ando algo cansado, pero el domingo te dire aun mas cosas hermosa")
+  )
   return (
     <div className="crt">
       <UpperWindow />
