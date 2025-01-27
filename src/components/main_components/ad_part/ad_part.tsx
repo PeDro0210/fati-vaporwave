e0e57e290f1313dda234c73d3367eec2d14c7419
Pedro Avila <pedroavila@Pedros-Laptop.local>
2025-01-26 22:22:31 -0600

    Fixing some rendering issues

--- src/components/main_components/ad_part/ad_part.tsx
+++ src/components/main_components/ad_part/ad_part.tsx
@@ -1,13 +1,14 @@
+import ResizableWindow from '../../usecases/resizable_window';
 import './ad_part.css';
 
 function AdPart() {
-  let url: string = "https://firebasestorage.googleapis.com/v0/b/fatipage-a0067.firebasestorage.app/o/get-in-loser-were-going-to-frutiger-aero-city-v0-16h5gw9xhe6c1%20(online-video-cutter.com).mp4?alt=media&token=f3d877ca-8dba-4423-b356-16d8db177d8f";
+  const url: string = "https://firebasestorage.googleapis.com/v0/b/fatipage-a0067.firebasestorage.app/o/get-in-loser-were-going-to-frutiger-aero-city-v0-16h5gw9xhe6c1%20(online-video-cutter.com).mp4?alt=media&token=f3d877ca-8dba-4423-b356-16d8db177d8f";
   return (
-    <div>
-      <a href="https://github.com/PeDro0210" target="_blank" rel="noopener noreferrer">
-        <video src={url} className="ad-side" loop autoPlay />
-      </a>
-    </div>
+    <div className='ad-side'>
+      < a href="https://github.com/PeDro0210" target="_blank" rel="noopener noreferrer" >
+        <ResizableWindow height="60vh" width="40vh" video={url} title="Ad.mp4" />
+      </a >
+    </div >
   )
 }
 
--- src/components/main_components/error/error_window.tsx
+++ src/components/main_components/error/error_window.tsx
@@ -3,8 +3,8 @@ import ResizableWindow from "../../usecases/resizable_window";
 
 
 function ErrorWindow() {
-  let url: string = "https://firebasestorage.googleapis.com/v0/b/fatipage-a0067.firebasestorage.app/o/MINDWAVE%20OST%20-%20Dorkus64%20-%20Although%20the%20Sky%20(Sped%20Up).mp4?alt=media&token=35e1c4d4-f18e-427e-ba5e-a438322188cc"
-  let url2: string = "https://firebasestorage.googleapis.com/v0/b/fatipage-a0067.firebasestorage.app/o/The%20Kitty%20Cat%20Dance.mp4?alt=media&token=2c3ffabc-47ec-423a-a011-90a3be835f29"
+  const url: string = "https://firebasestorage.googleapis.com/v0/b/fatipage-a0067.firebasestorage.app/o/MINDWAVE%20OST%20-%20Dorkus64%20-%20Although%20the%20Sky%20(Sped%20Up).mp4?alt=media&token=35e1c4d4-f18e-427e-ba5e-a438322188cc"
+  const url2: string = "https://firebasestorage.googleapis.com/v0/b/fatipage-a0067.firebasestorage.app/o/The%20Kitty%20Cat%20Dance.mp4?alt=media&token=2c3ffabc-47ec-423a-a011-90a3be835f29"
   return (
     <div className="upper-window">
       <ResizableWindow height="auto" width="auto" video={url} title="Sowwy :C" />
